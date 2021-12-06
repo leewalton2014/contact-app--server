3e328d0d5d453d7fe8e93d203113bae79e8b9f4e
Lee Walton <lee.walton2014@outlook.com>
2021-12-06 01:12:42 +0000

    delete contact methods

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -47,7 +47,9 @@ const ContactState = (props) => {
         dispatch({ type: ADD_CONTACT, payload: contact });
     };
     //Delete contact
-
+    const deleteContact = (id) => {
+        dispatch({ type: DELETE_CONTACT, payload: id });
+    };
     //Set current contact
 
     //Clear current contact
@@ -63,6 +65,7 @@ const ContactState = (props) => {
             value={{
                 contacts: state.contacts,
                 addContact,
+                deleteContact,
             }}
         >
             {props.children}
